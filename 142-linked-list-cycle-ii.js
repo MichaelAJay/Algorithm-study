c436cb188f4e822b829aa78a7c42a03aab60fc6d
mjay <michael@merged.group>
2022-08-27 22:07:20 -0400

    Added Map implementation

--- 142-linked-list-cycle-ii.js
+++ 142-linked-list-cycle-ii.js
@@ -7,23 +7,45 @@
  */
 
 /**
+ * Try 1: Just went for it
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+//  var detectCycle = function(head) {
+//     if (head === null || head.next === null) return null
+//     const anchor = head
+//     let maxChecks = 1
+//     while (head.next) {
+//         let innerList = anchor
+//         let checkCounter = 0
+//         while (checkCounter < maxChecks) {
+//             if (head.next === innerList) return innerList
+//             innerList = innerList.next
+//             ++checkCounter
+//         }
+//         head = head.next
+//         ++maxChecks
+//     }
+//     return null
+// };
+
+/**
+ * Try 2: After I read up on hash tables.
+ */
+/**
  * @param {ListNode} head
  * @return {ListNode}
  */
  var detectCycle = function(head) {
+    const collection = new Map();
     if (head === null || head.next === null) return null
-    const anchor = head
-    let maxChecks = 1
+    collection.set(head,null)
     while (head.next) {
-        let innerList = anchor
-        let checkCounter = 0
-        while (checkCounter < maxChecks) {
-            if (head.next === innerList) return innerList
-            innerList = innerList.next
-            ++checkCounter
-        }
+        if (collection.has(head.next)) return head.next
+        collection.set(head.next, null)
         head = head.next
-        ++maxChecks
     }
     return null
 };
