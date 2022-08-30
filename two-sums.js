fd4c45d895d03b537067b51d9a267b5e642743ee
mjay <michael@merged.group>
2022-08-29 21:43:52 -0400

    Removed conditional clause which was causing non-positive solutions to fail

--- two-sums.js
+++ two-sums.js
@@ -5,10 +5,8 @@
  */
  var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length - 1; i++) {
-        if (nums[i] <= target) {
-            for (let j = i + 1; j < nums.length; j++) {
-                if (nums[j] === target - nums[i]) return [i, j]
-            }
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] === target - nums[i]) return [i, j]
         }
     }
 };
